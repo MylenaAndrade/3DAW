af4f0b6454233c900f194b40b90c0ab77e5769e9
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-06-20 17:46:31 -0300

    Excluindo o dado e a variável id.
    Exclusão de todos os dados e variáveis id já que ele vai ser auto incrementado pelo banco de dados.

--- 3DAW-AV1/criarPergunta.php
+++ 3DAW-AV1/criarPergunta.php
@@ -5,7 +5,7 @@ include("conexao.php");
         $resposta1 = $_GET["resposta1"];
         $resposta2 = $_GET["resposta2"];
         $resposta3 = $_GET["resposta3"];
-        $id = $_GET["id"];
+        
         echo $pergunta;
         if(!file_exists("perguntasRespostas.txt")){
                 $arqPerg = fopen("perguntasRespostas.txt", "w") or die("erro ao criar arquivo");
