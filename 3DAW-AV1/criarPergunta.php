99e41a6fa5b504ed2b530ea606ae3196c0f7e363
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-06-20 18:03:09 -0300

    Alterando o formulário HTML.
    Alterando o formulário HTML para que as respostas sejam condizentes com as tabelas e colunas do banco de dados.

--- 3DAW-AV1/criarPergunta.php
+++ 3DAW-AV1/criarPergunta.php
@@ -5,7 +5,7 @@ include("conexao.php");
         $resposta1 = $_GET["resposta1"];
         $resposta2 = $_GET["resposta2"];
         $resposta3 = $_GET["resposta3"];
-
+        $resposta4 = $_GET["resposta4"];
         
         
     }
