f4fb86afa50e78c905254665493d95d65931ad43
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-06-20 18:18:42 -0300

    Inserindo a pergunta no database.
    Inserindo a pergunta na tabela perguntas do banco de dados.

--- 3DAW-AV1/criarPergunta.php
+++ 3DAW-AV1/criarPergunta.php
@@ -6,8 +6,23 @@ include("conexao.php");
         $resposta2 = $_GET["resposta2"];
         $resposta3 = $_GET["resposta3"];
         $resposta4 = $_GET["resposta4"];
+        $discursiva = '';
+
+        if($resposta1 == ''){
+            $discursiva = 'S';
+        }else{
+            $discursiva = 'N';
+        }
+
+        $sql ="INSERT INTO perguntas values(default,'$pergunta','$discursiva')";
         
-        
+        if(mysqli_query($mysqli, $sql)){
+            echo "Pergunta cadastrada com sucesso";
+        }else{
+            echo "Erro!" . mysqli_connect_error($mysqli);
+        }
+
+        mysqli_close($mysqli);
     }
 
         
