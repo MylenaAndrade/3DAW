ad9b2f7ce2dcd4111707743610882714fe60c6dd
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-03 18:59:24 -0300

    Listando os dados do database
    Trocando os comandos de listagem das perguntas de um arquivo .txt para um banco de dados.

--- 3DAW-AV1/index.php
+++ 3DAW-AV1/index.php
@@ -1,3 +1,16 @@
+<?php
+
+include_once('conexao.php');
+
+$query = "SELECT pergunta,discursiva,opcao_a,opcao_b,opcao_c, id FROM perguntas";
+
+$result = mysqli_query($mysqli,$query);
+	
+	if($result){
+		
+	
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -12,39 +25,40 @@
     <a class="criar-pergunta" href="criarPergunta.html">+ Criar Pergunta</a>
     <form action="">
         <br>
-        <?php if(file_exists("perguntasRespostas.txt")){
-            $arqPerg = fopen("perguntasRespostas.txt", "r");
-            $pergunta= explode(";", fgets($arqPerg));
-            $resp[]= 0; $x =0; 
-            while(!feof($arqPerg)){
-            if($pergunta[1] == -1){
-                $pergunta= explode(";", fgets($arqPerg))?>
-                <label for=""><?php echo $x+1 . ") " . $pergunta[1]?></label>
+        <?php 
+            while($row = mysqli_fetch_array($result)){
+                    $pergunta = $row["pergunta"];
+                    $id = $row["id"];
+        ?>  
+                <label for=""><?php echo $pergunta;?></label>
+                <br>
+                <?php 
+                    $disc = $row["discursiva"];
+                    if($disc == 'S'){
+            ?>
+                <input type="text" name="discursiva" id="discursiva">
                 <br><br>
-                <textarea name="discursiva" id="discursiva" cols="20" rows="3"></textarea>
-                <br><br>
-                <a class="botao-alterar" href="alterarPergunta.html?id=<?php echo $pergunta[0]?>">Alterar</a>
-                <a class="botao-remover" href="removerPergunta.php?id=<?php echo $pergunta[0]?>">Remover</a>
-                <br><br>
-           <?php }else{ 
-                $resposta = explode(";", fgets($arqPerg));?>
-                <label for=""><?php echo $x+1 . ") " . $pergunta[1]?></label>
-                <br><br>
-                <div class="multipla-escolha">
-                    <input type="radio" name="<?php echo $resp[$x]?>" value="resp1"><?php echo $resposta[1]?>
+                <?php }else{ 
+                    $opA = $row["opcao_a"];
+                    $opB = $row["opcao_b"];
+                    $opC = $row["opcao_c"];?>
+                    <input type="radio" name="<?php echo $pergunta?>" value="resp1"><?php echo $opA?>
                     <br>
-                    <input type="radio" name="<?php echo $resp[$x]?>" value="resp2"><?php echo $resposta[2]?>
+                    <input type="radio" name="<?php echo $pergunta?>" value="resp2"><?php echo $opB?>
                     <br>
-                    <input type="radio" name="<?php echo $resp[$x]?>" value="resp3"><?php echo $resposta[3]?>
+                    <input type="radio" name="<?php echo $pergunta?>" value="resp3"><?php echo $opC?>
                     <br><br>
-                </div>
-                    <a class="botao-alterar" href="alterarPergunta.html?id=<?php echo $resposta[0]?>">Alterar</a>
-                    <a class="botao-remover" href="removerPergunta.php?id=<?php echo $resposta[0]?>">Remover</a>
-                    <br><br>
+                    <?php } ?>
+                <a class="botao-alterar" href="alterarPergunta.html?id=<?php echo $id?>">Alterar</a>
+                <a class="botao-remover" href="removerPergunta.php?id=<?php echo $id?>">Remover</a>
+                <br><br>
+   
+                    
+            <?php
+            }
+        }
+            ?>        
                 
-        <?php } ; $x++; $resp[$x]=$x; $pergunta = explode(";", fgets($arqPerg));
-    } fclose($arqPerg); }else{echo "NÃO EXISTE PERGUNTAS!";}
-    ?>
             <br>
             <input class="botao-enviar" type="submit" value="Enviar">
         
