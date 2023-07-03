43cbaf52285b12646417626e29f212dbd5b22f52
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-03 19:00:32 -0300

    Trocando os comandos de exclusão
    Trocando os comandos de "exclusão" de um arquivo .txt para um comando de MySQL.

--- 3DAW-AV1/removerPergunta.php
+++ 3DAW-AV1/removerPergunta.php
@@ -1,37 +1,11 @@
 <?php 
-$id = ""; 
+include_once('conexao.php');
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
     
     $id=$_GET["id"];
-    $arqPerg = fopen("perguntasRespostas.txt", "r");
-    $arquivoNovo = "";
-    $arquivoNomeVelho = "perguntasResposta.txt";
-    $arquivoNomeNovo ="perguntasRespostas.txt";
-    while(!feof($arqPerg)){
-        $linhas[] = fgets($arqPerg);
-    }
-
-    fclose($arqPerg);
-    
-    foreach($linhas as $linha){
-        $colunaDados = explode(";", $linha);
-        if($colunaDados[0] != $id){
-            if(!file_exists("perguntasResposta.txt")){
-                $arquivoNovo = fopen("perguntasResposta.txt", "w") or die("erro ao criar arquivo");
-                fwrite($arquivoNovo, $linha);
-                fclose($arquivoNovo);
-            }else{
-                $arquivoNovo = fopen("perguntasResposta.txt", "a");
-                fwrite($arquivoNovo,$linha);
-                fclose($arquivoNovo);
-            }
-        }
-        
-    }
-    
-    unlink("perguntasRespostas.txt");
-    rename($arquivoNomeVelho, $arquivoNomeNovo);
+    $query = "DELETE FROM perguntas WHERE id = '$id'";
+    mysqli_query($mysqli,$query);
     header("location: /3DAW/3DAW/3DAW-AV1/index.php");
     exit;
 }
