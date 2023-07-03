180de65f706fc10001fdad6d8de0d372b38fb817
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-03 20:08:19 -0300

    Trocando os comandos de alteração.
    Trocando os comandos de alteração que estavam para arquivos .txt, e agora foram modificados para interação com o  database.

--- 3DAW-AV1/alterarPergunta.php
+++ 3DAW-AV1/alterarPergunta.php
@@ -1,52 +1,47 @@
 <?php 
+    include_once('conexao.php');
     if($_SERVER["REQUEST_METHOD"] == "GET"){
         
         $id=$_GET["id"];
-        $arqPerg = fopen("perguntasRespostas.txt", "r");
-        $arquivoNovo = "";
-        $arquivoNomeVelho = "perguntasResposta.txt";
-        $arquivoNomeNovo ="perguntasRespostas.txt";
-        while(!feof($arqPerg)){
-            $linhas[] = fgets($arqPerg);
-        }
-
-        fclose($arqPerg);
-        
-        foreach($linhas as $linha){
-            $colunaDados = explode(";", $linha);
-            if($colunaDados[0] != $id){
-                if(!file_exists("perguntasResposta.txt")){
-                    $arquivoNovo = fopen("perguntasResposta.txt", "w") or die("erro ao criar arquivo");
-                    fwrite($arquivoNovo, $linha);
-                    fclose($arquivoNovo);
-                }else{
-                    $arquivoNovo = fopen("perguntasResposta.txt", "a");
-                    fwrite($arquivoNovo,$linha);
-                    fclose($arquivoNovo);
-                }
-                
-            }
-        }
-
-        unlink("perguntasRespostas.txt");
-        rename($arquivoNomeVelho, $arquivoNomeNovo);
-
+        echo 'oi';
         $pergunta = $_GET["pergunta"];
         $resposta1 = $_GET["resposta1"];
         $resposta2 = $_GET["resposta2"];
         $resposta3 = $_GET["resposta3"];
+        $resposta4 = $_GET["resposta4"];
+        if(($resposta1 == "") && ($resposta2 == "") && ($resposta3 == "") && ($resposta4 == "")){
+             $discursiva = "S";
+         }else{
+             $discursiva = "N";
+             if($pergunta == ""){
+                $query = "SELECT pergunta FROM perguntas where id = '$id'";
+                 $pergunta = mysqli_query($mysqli,$query);
+             }
+             if($resposta1 == ""){
+                 $query = "SELECT opcao_a FROM perguntas where id = '$id'";
+                 $resposta1 = mysqli_query($mysqli,$query);
+             }
 
-        $arqPerg = fopen("perguntasRespostas.txt", "a");
+             if($resposta2 == ""){
+                 $query = "SELECT opcao_b FROM perguntas where id = '$id'";
+                 $resposta2 = mysqli_query($mysqli,$query);
+             }
 
-        if($resposta1 == ""){
-            $linha = "\n" . $id. ";" . "-1". "\n" . $id . ";" . $pergunta;
-        }else{
-            $linha = "\n" . $id. ";". $pergunta . "\n" . $id .";".$resposta1 . ";" . $resposta2 . ";" . $resposta3;
-        }
-        fwrite($arqPerg,$linha);
-        fclose($arqPerg);
-        
-        header("location: /Trabalhos/3DAW/3DAW-AV1/index.php");
+             if($resposta3 == ""){
+                 $query = "SELECT opcao_c FROM perguntas where id = '$id'";
+                 $resposta3 = mysqli_query($mysqli,$query);
+             }
+
+             if($resposta4 == ""){
+                 $query = "SELECT respMult FROM perguntas where id = '$id'";
+                 $resposta3 = mysqli_query($mysqli,$query);
+             }
+
+            }
+            $query = "UPDATE perguntas SET pergunta = '$pergunta' , discursiva = '$discursiva' , respMult = '$resposta4' , opcao_a = '$resposta1', opcao_b = '$resposta2' , opcao_c = '$resposta3' WHERE id = '$id'";
+            mysqli_query($mysqli, $query);
+            mysqli_close($mysqli);
+        header("location: /Trabalhos/3DAW/3DAW/3DAW-AV1/index.php");
         exit;
     }
 ?>
