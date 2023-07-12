f2dc169042382c2acfa94615ca6c8ea4d5e25960
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-12 10:37:38 -0300

    Melhorando a estilização da página. pt2

--- 3DAW-AV2/index.php
+++ 3DAW-AV2/index.php
@@ -16,6 +16,9 @@ $result = mysqli_query($mysqli,$query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ysabeau+Infant:ital@1&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <title>Concurso KeyFalls</title>
 </head>
@@ -43,23 +46,32 @@ $result = mysqli_query($mysqli,$query);
                     echo"$nome "; }?>
         <div class="tabela"> 
         <table>
-    	<tr>
+        <tr>
           <td>Nome</td>
           <td>CPF</td>
+          <td>Identidade</td>
+          <td>Email</td>
+          <td>Cargo Pretendido</td>
           <td>Ação</td>
         </tr>
         <?php 
-                $query = "SELECT id,nome,cpf FROM candidatos WHERE sala = '$sala' ORDER BY nome;";
+                $query = "SELECT id,nome,cpf,identidade,email,cargo FROM candidatos WHERE sala = '$sala' ORDER BY nome;";
 
                 $candidatos = mysqli_query($mysqli,$query);
                 while($linha = mysqli_fetch_array($candidatos)){
                     $nome = $linha["nome"];
                     $cpf = $linha["cpf"];
+                    $identidade = $linha["identidade"];
+                    $email = $linha["email"];
+                    $cargo = $linha["cargo"];
                     $id = $linha["id"];?>
                    
         <tr>
         <th><?php echo "$nome"?></th>
         <th><?php echo "$cpf"?></th>
+        <th><?php echo "$identidade"?></th>
+        <th><?php echo "$email"?></th>
+        <th><?php echo "$cargo"?></th>
         <th>
             <a href="alterarSala.html?cpf=<?php echo $cpf?>"><img src="img/lapis.png" alt="Alterar Sala"></a>
             <a href="removerCandidato.php?id=<?php echo $id;?>"><img src="img/lixo.png" alt="Remover Candidato"></a>
